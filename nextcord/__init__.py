604f95a36203dbb201aed240c056b220d63ebfec
rawiphat <47816022+rawiphat@users.noreply.github.com>
2025-06-27 00:08:37 +0700

    Add files via upload

---
+++ nextcord/__init__.py
@@ -0,0 +1,13 @@
+from .client import *
+from .errors import *
+from .guild import *
+from .user import *
+from .message import *
+from .channel import *
+from .embeds import *
+from .mentions import *
+from .permissions import *
+from .webhook import *
+from .utils import *
+from .ext import commands, tasks
+__version__ = "2.5.0"
---
+++ nextcord/client.py
@@ -0,0 +1,2 @@
+from .user import ClientUser
+class Client: pass
