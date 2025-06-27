bf0c0baffa8a63c722ccac60793b6e9f80af0167
rawiphat <47816022+rawiphat@users.noreply.github.com>
2025-06-27 16:22:08 +0700

    Update admin_web.py

--- admin_web.py
+++ admin_web.py
@@ -1,1 +1,12 @@
-print("✅ Admin web is ready (placeholder)")
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '✅ Admin web is working'
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
