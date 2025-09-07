cc132783e762e8670fca8509ad3b6a7c960f6941
rawiphat <47816022+rawiphat@users.noreply.github.com>
2025-09-08 02:36:59 +0700

    Update log.js

--- log.js
+++ log.js
@@ -1,3 +1,9 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("Bot is running"));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 require("dotenv").config();
 const { 
     Client, 
